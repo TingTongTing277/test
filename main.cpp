775423add55642cf6e1a98b4fe3d163aedd4c51f
TingTongTing277 <3484351851@qq.com>
2026-08-29 17:56:01 +0800

    合并排序程序与输出字符功能

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
 int main() {
     printf("GoodBye, World!\n");
+
+    int a[10];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + 10);
+
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
